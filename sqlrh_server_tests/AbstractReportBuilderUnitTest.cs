a469ae13aba2251347919f8ea0586884233ffaf8
al-al-se <al.al.se@yandex.ru>
2021-05-31 16:51:52 +0300

    third test is added, but refactoring is needed: replace static method sqlquery executor to interface

--- sqlrh_server_tests/AbstractReportBuilderUnitTest.cs
+++ sqlrh_server_tests/AbstractReportBuilderUnitTest.cs
@@ -2,6 +2,7 @@ using System.IO;
 using System;
 using Xunit;
 using Moq;
+using System.Threading.Tasks;
 
 namespace sqlrh_server_tests
 {
@@ -62,6 +63,40 @@ namespace sqlrh_server_tests
 
             File.Delete(srcFileName);
 
+        }
+
+
+        [Fact]
+        public void ExecuteQueryTest()
+        {
+            string query = "s(,4:f1) db1 select min(value) from table_a";
+
+            var dbMock = new Mock<IExternalDataBaseRepository>();
+            dbMock.Setup(a => a.GetConnectionString(It.IsNotNull<string>())).
+                Returns(new Task<string>(() => "db1_cs"));
+
+            TxtReportBuilder tb = new TxtReportBuilder(dbMock.Object);
+
+            string srcFileName = Path.Combine(Directory.GetCurrentDirectory(),"src3.txt");
+            
+            using (var src_file = new StreamWriter(srcFileName)) 
+            {
+
+            }
+
+            using (var src = new StreamReader(srcFileName))
+            {
+                using (var dst = new StringWriter())
+                {
+                    tb.OpenFilesForMock(src,dst);
+
+                    string obtained_result = dst.ToString();
+                    string expected_result = "12";
+                    Assert.Equal(obtained_result,expected_result);
+                }
+            }
+
+            File.Delete(srcFileName);
         }
     }
 }
